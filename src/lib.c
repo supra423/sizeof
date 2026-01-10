f3e94acfdc6bd91b39456e6e5255b8a44203ae71
supra <alexusenzoroa@gmail.com>
2026-01-11 01:32:24 +0800

    added clang-format lol

--- src/lib.c
+++ src/lib.c
@@ -5,97 +5,97 @@
 #include <sys/stat.h>
 
 char *truncate_file_name(char filename[]) {
-    int c = 0;
-    bool contains_dir = false;
-    while (filename[c] != '\0') {
-        c++;
-        if (filename[c] == '/' && !contains_dir) {
-            contains_dir = true;
-        }
-    } // after this, filename[c] should point at the last char of filename
+	int c = 0;
+	bool contains_dir = false;
+	while (filename[c] != '\0') {
+		c++;
+		if (filename[c] == '/' && !contains_dir) {
+			contains_dir = true;
+		}
+	} // after this, filename[c] should point at the last char of filename
 
-    if (!contains_dir) {
-        return NULL;
-    }
+	if (!contains_dir) {
+		return NULL;
+	}
 
-    while (filename[c] != '/') {
-        // find the last occurence of '/' by backtracking
-        c--;
-    }
-    int idx_of_last_slash = c;
-    c++; // increment to point after '/'
-    int new_str_size = 0;
+	while (filename[c] != '/') {
+		// find the last occurence of '/' by backtracking
+		c--;
+	}
+	int idx_of_last_slash = c;
+	c++; // increment to point after '/'
+	int new_str_size = 0;
 
-    while (filename[c] != '\0') { // determine size of new string
-        c++;
-        new_str_size++;
-    }
+	while (filename[c] != '\0') { // determine size of new string
+		c++;
+		new_str_size++;
+	}
 
-    char *new_str = malloc(new_str_size + 1); // + 1 for the '\0'
-    for (int i = 0; i < new_str_size; i++) {
-        new_str[i] = filename[i + idx_of_last_slash + 1];
-    }
-    new_str[new_str_size] = '\0';
-    return new_str;
+	char *new_str = malloc(new_str_size + 1); // + 1 for the '\0'
+	for (int i = 0; i < new_str_size; i++) {
+		new_str[i] = filename[i + idx_of_last_slash + 1];
+	}
+	new_str[new_str_size] = '\0';
+	return new_str;
 }
 
 void display_help() {
-    printf(
-        "\nChecks logical file size in Bytes, KiB, MiB, and GiB\n"
-        "Amount of varying units depends on file size\n"
-        "If file size is greater than 1 MiB, it should output size in:\n"
-        "B, KiB, and MiB\n"
-        "but if file size is only lesser than 1 KiB, it should only\n"
-        "output size in Bytes.\n"
-        "This program can accept multiple files as arguments.\n\n"
-        "Example usage: \n\n"
-        "$ sizeof foo.h dir/bar.c\n\n"
-        "OUTPUT:\n\n"
-        "FILE: foo.h\n"
-        "foo.h IN BYTES: 123B\n\n"
-        "FILE: dir/bar.c\n"
-        "dir/bar.c IN BYTES: 1234B\n"
-        "dir/bar.c IN KiB: 1.23KiB\n\n"
-        "OPTIONS:\n"
-        "\n\"--help / -h\" - to display this message\n"
-        "\n\"--truncate-filename / -tf\" - to truncate file name, e.g.,\n"
-        "\t\"dir/bar.c\" to \"bar.c\" only.\n\n"
-        "WARNING:\n\nMake sure that the arguments you enter that are\n"
-        "intended to be used as a file name or path don't start with: '-'\n"
-        "the program will misinterpret it as an option. It's pretty unlikely\n"
-        "to happen though because who the hell names files/directories\n"
-        "prefixed with a '-'.\n");
+	printf(
+		"\nChecks logical file size in Bytes, KiB, MiB, and GiB\n"
+		"Amount of varying units depends on file size\n"
+		"If file size is greater than 1 MiB, it should output size in:\n"
+		"B, KiB, and MiB\n"
+		"but if file size is only lesser than 1 KiB, it should only\n"
+		"output size in Bytes.\n"
+		"This program can accept multiple files as arguments.\n\n"
+		"Example usage: \n\n"
+		"$ sizeof foo.h dir/bar.c\n\n"
+		"OUTPUT:\n\n"
+		"FILE: foo.h\n"
+		"foo.h IN BYTES: 123B\n\n"
+		"FILE: dir/bar.c\n"
+		"dir/bar.c IN BYTES: 1234B\n"
+		"dir/bar.c IN KiB: 1.23KiB\n\n"
+		"OPTIONS:\n"
+		"\n\"--help / -h\" - to display this message\n"
+		"\n\"--truncate-filename / -tf\" - to truncate file name, e.g.,\n"
+		"\t\"dir/bar.c\" to \"bar.c\" only.\n\n"
+		"WARNING:\n\nMake sure that the arguments you enter that are\n"
+		"intended to be used as a file name or path don't start with: '-'\n"
+		"the program will misinterpret it as an option. It's pretty unlikely\n"
+		"to happen though because who the hell names files/directories\n"
+		"prefixed with a '-'.\n");
 }
 
 void display_output(char *final_file_name, double size) {
-    printf("\nFILE: %s\n", final_file_name);
-    if (size <= BYTES_IN_KIB) {
-        printf("%s IN BYTES: %.0fB\n", final_file_name, size);
-    } else if (size <= BYTES_IN_MIB) {
-        const double size_in_kib = size / BYTES_IN_KIB;
+	printf("\nFILE: %s\n", final_file_name);
+	if (size <= BYTES_IN_KIB) {
+		printf("%s IN BYTES: %.0fB\n", final_file_name, size);
+	} else if (size <= BYTES_IN_MIB) {
+		const double size_in_kib = size / BYTES_IN_KIB;
 
-        printf("%s IN BYTES: %.0fB\n"
-               "%s IN KiB: %.2fKiB\n",
-               final_file_name, size, final_file_name, size_in_kib);
-    } else if (size <= BYTES_IN_GIB) {
-        const double size_in_kib = size / BYTES_IN_KIB;
-        const double size_in_mib = size / BYTES_IN_MIB;
+		printf("%s IN BYTES: %.0fB\n"
+			   "%s IN KiB: %.2fKiB\n",
+			   final_file_name, size, final_file_name, size_in_kib);
+	} else if (size <= BYTES_IN_GIB) {
+		const double size_in_kib = size / BYTES_IN_KIB;
+		const double size_in_mib = size / BYTES_IN_MIB;
 
-        printf("%s IN BYTES: %.0fB\n"
-               "%s IN KiB: %.2fKiB\n"
-               "%s IN MiB: %.2fMiB\n",
-               final_file_name, size, final_file_name, size_in_kib,
-               final_file_name, size_in_mib);
-    } else {
-        const double size_in_kib = size / BYTES_IN_KIB;
-        const double size_in_mib = size / BYTES_IN_MIB;
-        const double size_in_gib = size / BYTES_IN_GIB;
+		printf("%s IN BYTES: %.0fB\n"
+			   "%s IN KiB: %.2fKiB\n"
+			   "%s IN MiB: %.2fMiB\n",
+			   final_file_name, size, final_file_name, size_in_kib,
+			   final_file_name, size_in_mib);
+	} else {
+		const double size_in_kib = size / BYTES_IN_KIB;
+		const double size_in_mib = size / BYTES_IN_MIB;
+		const double size_in_gib = size / BYTES_IN_GIB;
 
-        printf("%s IN BYTES: %.0fB\n"
-               "%s IN KiB: %.2fKiB\n"
-               "%s IN MiB: %.2fMiB\n"
-               "%s IN GiB: %.2fGiB\n",
-               final_file_name, size, final_file_name, size_in_kib,
-               final_file_name, size_in_mib, final_file_name, size_in_gib);
-    }
+		printf("%s IN BYTES: %.0fB\n"
+			   "%s IN KiB: %.2fKiB\n"
+			   "%s IN MiB: %.2fMiB\n"
+			   "%s IN GiB: %.2fGiB\n",
+			   final_file_name, size, final_file_name, size_in_kib,
+			   final_file_name, size_in_mib, final_file_name, size_in_gib);
+	}
 }
--- src/main.c
+++ src/main.c
@@ -9,88 +9,88 @@
 static bool truncate_flag = false;
 
 void evaluate_options(char *argv[], int i) {
-    if (*argv[i] == '-') {
-        // if argument is prefixed with '-' it might be an option
-        if (argv[i] == NULL || strcmp(argv[i], "--help") == 0 ||
-            strcmp(argv[i], "-h") == 0) {
-            display_help();
-            exit(0);
-        } else if (strcmp(argv[i], "--truncate-filename") == 0 ||
-                   strcmp(argv[i], "-tf") == 0) {
-            truncate_flag = true;
-        } else {
-            printf("\"%s\" option doesn't exist, check for typos!\n", argv[i]);
-            exit(1);
-        }
-    }
+	if (*argv[i] == '-') {
+		// if argument is prefixed with '-' it might be an option
+		if (argv[i] == NULL || strcmp(argv[i], "--help") == 0 ||
+			strcmp(argv[i], "-h") == 0) {
+			display_help();
+			exit(0);
+		} else if (strcmp(argv[i], "--truncate-filename") == 0 ||
+				   strcmp(argv[i], "-tf") == 0) {
+			truncate_flag = true;
+		} else {
+			printf("\"%s\" option doesn't exist, check for typos!\n", argv[i]);
+			exit(1);
+		}
+	}
 }
 
 int evaluate_st_mode(struct stat buf, char *file_name) {
-    if (S_ISDIR(buf.st_mode)) {
-        printf("\"%s\" is a directory! Cannot calculate size\n", file_name);
-        return -1;
-    }
+	if (S_ISDIR(buf.st_mode)) {
+		printf("\"%s\" is a directory! Cannot calculate size\n", file_name);
+		return -1;
+	}
 
-    if (!S_ISREG(buf.st_mode)) {
-        printf("File: \"%s\" file type is not recognized!\n", file_name);
-        return -1;
-    }
-    return 0;
+	if (!S_ISREG(buf.st_mode)) {
+		printf("File: \"%s\" file type is not recognized!\n", file_name);
+		return -1;
+	}
+	return 0;
 }
 
 void process_output(char *file_name, double size) {
-    bool is_mallocd = false;
-    char *final_file_name;
-    if (truncate_flag) {
-        final_file_name = truncate_file_name(file_name);
-        is_mallocd = true;
-        if (final_file_name == NULL) {
-            final_file_name = file_name;
-            is_mallocd = false;
-        }
-    } else
-        final_file_name = file_name;
+	bool is_mallocd = false;
+	char *final_file_name;
+	if (truncate_flag) {
+		final_file_name = truncate_file_name(file_name);
+		is_mallocd = true;
+		if (final_file_name == NULL) {
+			final_file_name = file_name;
+			is_mallocd = false;
+		}
+	} else
+		final_file_name = file_name;
 
-    display_output(final_file_name, size);
+	display_output(final_file_name, size);
 
-    if (is_mallocd)
-        free(final_file_name);
+	if (is_mallocd)
+		free(final_file_name);
 }
 
 int process_args(int argc, char *argv[]) {
-    if (argc == 1) {
-        display_help();
-        return 0;
-    }
-    for (int i = 1; i < argc; i++) {
-        evaluate_options(argv, i);
-        char *file_name = argv[i];
+	if (argc == 1) {
+		display_help();
+		return 0;
+	}
+	for (int i = 1; i < argc; i++) {
+		evaluate_options(argv, i);
+		char *file_name = argv[i];
 
-        if (*file_name == '-')
-            // skip arg if it is an option determined by the prefix '-'
-            // this avoids treating the arg as a file
-            continue;
+		if (*file_name == '-')
+			// skip arg if it is an option determined by the prefix '-'
+			// this avoids treating the arg as a file
+			continue;
 
-        if (access(file_name, F_OK) != 0) {
-            printf("File \"%s\" doesn't exist, check for typos!\n", file_name);
-            continue;
-        }
+		if (access(file_name, F_OK) != 0) {
+			printf("File \"%s\" doesn't exist, check for typos!\n", file_name);
+			continue;
+		}
 
-        // stat struct contains info about the file like size, file type, etc.
-        struct stat buf;
-        stat(file_name, &buf);
+		// stat struct contains info about the file like size, file type, etc.
+		struct stat buf;
+		stat(file_name, &buf);
 
-        if (evaluate_st_mode(buf, file_name) == -1)
-            continue;
+		if (evaluate_st_mode(buf, file_name) == -1)
+			continue;
 
-        // st_size stores file size in bytes defined in sys/stat.h
-        const double size = buf.st_size;
+		// st_size stores file size in bytes defined in sys/stat.h
+		const double size = buf.st_size;
 
-        if (size != -1) {
-            process_output(file_name, size);
-        }
-    }
-    return 0;
+		if (size != -1) {
+			process_output(file_name, size);
+		}
+	}
+	return 0;
 }
 
 int main(int argc, char *argv[]) { return process_args(argc, argv); }
